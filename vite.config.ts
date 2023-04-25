e4f2d0d70634c5d30d994574f83dfc6cce4802ba
Tiago <sathoskath@gmail.com>
2023-04-25 19:27:03 -0300

    another fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      external: ['src/assets/**/*'],
+    },
+  },
 })
